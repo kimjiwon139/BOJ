ab07c142a3aa2aa03ff3ec528204c03db1eac491
kimjiwon139 <144809062+kimjiwon139@users.noreply.github.com>
2025-07-10 18:39:05 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.c
+++ 백준/Bronze/2753. 윤년/윤년.c
@@ -3,14 +3,10 @@
 int main(void) {
 	int year;
 	scanf("%d", &year);
-	if (year % 4 == 0) {
-		if (year % 400 == 0 || year % 100 != 0) {
-			printf("%d", 1);
-		}
-		else {
-			printf("%d", 0);
-		}
-}
+	
+	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+		printf("%d", 1);
+	}
 	else {
 		printf("%d", 0);
 	}
